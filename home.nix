6831cb37470807e9d328b0b5568f1cc75345771c
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2025-09-02 20:16:22 +0530

    s

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@ in
   home.activation.quickshell = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
   mkdir -p $HOME/.config/quickshell/default
   cp -r ${./config/quickshell/default}/* $HOME/.config/quickshell/default/
+  chmod -R u+rw $HOME/.config/quickshell/default
 '';
 
 
