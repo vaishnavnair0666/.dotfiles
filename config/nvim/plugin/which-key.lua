0c9f08b377fd971c474049074d9479d6b3e36f41
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2025-09-04 23:10:30 +0530

    which-key fixed

--- config/nvim/plugin/which-key.lua
+++ config/nvim/plugin/which-key.lua
@@ -7,7 +7,7 @@ require("which-key").setup {
       suggestions = 20,
     },
   },
-  window = {
+  win= {
     border = "rounded",
     position = "bottom",
     margin = { 1, 0, 1, 0 },
@@ -15,12 +15,24 @@ require("which-key").setup {
   },
 }
 
--- Example keymaps
 local wk = require("which-key")
-wk.register({
-  ["<leader>f"] = { name = "+file" },
-  ["<leader>g"] = { name = "+git" },
-  ["<leader>l"] = { name = "+lsp" },
-  ["<leader>t"] = { name = "+telescope" },
-}, { prefix = "<leader>" })
-
+wk.add({
+  { "<leader>f", group = "file" }, -- group
+  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+  { "<leader>fb", function() print("hello") end, desc = "Foobar" },
+  { "<leader>fn", desc = "New File" },
+  { "<leader>f1", hidden = true }, -- hide this keymap
+  { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+  { "<leader>b", group = "buffers", expand = function()
+      return require("which-key.extras").expand.buf()
+    end
+  },
+  {
+    -- Nested mappings are allowed and can be added in any order
+    -- Most attributes can be inherited or overridden on any level
+    -- There's no limit to the depth of nesting
+    mode = { "n", "v" }, -- NORMAL and VISUAL mode
+    { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+    { "<leader>w", "<cmd>w<cr>", desc = "Write" },
+  }
+})
